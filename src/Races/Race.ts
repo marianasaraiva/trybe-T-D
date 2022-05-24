5079b60d9c14f4a4c1e68e8f963cb245475eef2b
Mariana Saraiva <marianascm8@gmail.com>
2022-05-24 17:17:12 -0300

    fix: uptade private to protected atributtes class Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,6 +1,6 @@
 abstract class Race {
-  private _name: string;
-  private _dexterity: number;
+  protected _name: string;
+  protected _dexterity: number;
 
   constructor(name: string, dexterity: number) {
     this._name = name;
