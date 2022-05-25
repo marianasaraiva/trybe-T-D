0086cebefc56fecfe0e8352063cf954f3cd81a44
Mariana Saraiva <marianascm8@gmail.com>
2022-05-25 15:21:54 -0300

    fix: fight PVP class requirement 10

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -15,8 +15,10 @@ class PVP extends Battle {
   }
 
   fight(): number {
-    this._character1.attack(this._character2);
-    this._character2.attack(this._character1);
+    if (this._character1.lifePoints > 0 || this._character2.lifePoints > 0) {
+      this._character1.attack(this._character2);
+      this._character2.attack(this._character1);
+    }
     // console.log(super.fight()); // retorna 1 ou -1 
     return super.fight();
   }
